54e62867c2f5794ea8f4e10bc22ac84f6a1df036
Joel Linn <jl@conductive.de>
2019-06-14 14:56:29 +0200

    Disable screensaver

--- startup.sh
+++ startup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# auf netzwerk warten
+# Bildschirmschoner ausschalten
+xset s noblank 
+xset s off 
+xset -dpms
+
+# auf netzwerk warten und IP Anzeigen
 sleep 5
 zenity --info --width=200 --timeout=5 --title="IP Adresse" --text="IP Adresse:\n$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')"
 
 # mouse weg bewegen -nach- browser start
 sleep 20 && xdotool mousemove 10000 10000 &
 
-#browser starten
+# Browser starten
 exec /usr/bin/chromium-browser --kiosk https://www.divera247.com/monitor/3.html
